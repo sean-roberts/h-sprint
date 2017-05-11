a2fefc7db5419938bb9c5ba9ca12d335a2a3cee9
Sean Roberts <sean.roberts90@gmail.com>
2017-05-11 10:26:47 -0400

    sprint update plus better redirecting

--- serve.js
+++ serve.js
@@ -1,6 +1,11 @@
-const URL = 'https://github.com/orgs/hypothesis/projects/11';
+const URL = 'https://github.com/orgs/hypothesis/projects/13';
 
 module.exports = (req, res) => {
-    res.writeHead(301, { 'Location': URL });
+    res.writeHead(302, {
+        'Location': URL,
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': 0
+    });
     res.end();
 };
